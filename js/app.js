fa74a6d37c46e3ba76751477d8ba38eb8f1a0b6c
Matthew Malone <matthewmalon@umass.edu>
2019-03-26 13:22:45 -0400

    regex

--- js/app.js
+++ js/app.js
@@ -9,14 +9,19 @@ function getPredictions() {
 
     $.getJSON("/js/word-index.json", function(json) {
     function getFeatures(json, movieName){
+		
+		movieName = movieName.replace(/[\W_]+/g," ");
+		console.log(movieName);
         const arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; 
         let words = movieName.toLowerCase().trim().split(/\s+/);
         let i = arr.length-words.length;
         if(i < 0) i = 0;
         for (var j = 0; j < words.length; j++){
             arr[i] = json['word_index'][words[j]];
+            if (arr[i] == null) arr[i] = 0; 
             i++;
         }
+		console.log(arr);
         return [arr];
     }
 
@@ -35,12 +40,13 @@ function getPredictions() {
         var prediction = Array.from(output.dataSync());
         var probs = prediction_to_probabilities(prediction);
         prediction = prediction.sort().reverse();
-        console.log(probs[prediction[0]]);
-        var text = document.createTextNode("I think your movie has the genres: " 
+        
+		var text = document.createTextNode("I think your movie has the genres: " 
         + probs[prediction[0]] 
         + ", " + probs[prediction[1]]
         + ", " + probs[prediction[2]]);
         var para = document.createElement("p");
+		
         para.appendChild(text);
         var predArea = document.getElementById('prediction');
         while (predArea.firstChild) {
